51b262f1f104b9b4e1dfeb3f432acb1589267823
Rootjhon <354888562@qq.com>
2018-08-21 15:22:25 +0800

    android & ios for editor

--- CI/travis/Unity_install.sh
+++ CI/travis/Unity_install.sh
@@ -1,16 +1,49 @@
 #!/bin/bash
 
+# Make cache dir
 if [ ! -d "$HOME/cache" ]; then
     mkdir -m 777 "$HOME/cache"
 fi
 
-URL="https://netstorage.unity3d.com/unity/a9f86dcd79df/MacEditorInstaller/Unity-2017.3.0f3.pkg"
-FILENAME=$(basename "$URL")
+BASE_URL=http://netstorage.unity3d.com/unity
+VERSION="2017.3.0f3"
 
-if [ ! -f "$HOME/cache/$FILENAME" ]; then
-    echo "Downloading Unity"
-    curl --retry 5 -o "$HOME/cache/$FILENAME" "$URL"
-fi
+# Find hash on the html of the unity archives
+tmp=$(curl -s -i -X GET https://unity3d.com/fr/get-unity/download/archive)
+re="https:\/\/netstorage\.unity3d\.com\/unity\/(.+?)\/MacEditorInstaller\/Unity-$VERSION\.pkg"
+echo $re
 
-echo "Installing Unity"
-sudo installer -dumplog -package "$HOME/cache/$FILENAME" -target /
+HASH=a9f86dcd79df
+
+echo "version:$VERSION - hash:$HASH"
+
+function download()
+{
+	package="$1"
+	url="$BASE_URL/$HASH/$package"
+
+	FILENAME=$(basename "$package")
+
+
+	if [ ! -f "$HOME/cache/$FILENAME" ]; then
+		echo "Downloading Unity from $url: "
+		curl --retry 5 -o "$HOME/cache/$FILENAME" "$url"
+	fi
+}
+
+function install() 
+{
+	package="$1"
+	download "$package"
+
+	echo "Installing "$(basename "$package")
+	sudo installer -dumplog -package $(basename "$package") -target /
+}
+
+# See $BASE_URL/$HASH/unity-$VERSION-$PLATFORM.ini for complete list
+install "MacEditorInstaller/Unity-$VERSION.pkg"
+install "MacEditorTargetInstaller/UnitySetup-Mac-Support-for-Editor-$VERSION.pkg"
+install "MacEditorTargetInstaller/UnitySetup-iOS-Support-for-Editor-$VERSION.pkg"
+install "MacEditorTargetInstaller/UnitySetup-Android-Support-for-Editor-$VERSION.pkg"
+install "MacEditorTargetInstaller/UnitySetup-Windows-Support-for-Editor-$VERSION.pkg"
+install "MacEditorTargetInstaller/UnitySetup-Linux-Support-for-Editor-$VERSION.pkg"
